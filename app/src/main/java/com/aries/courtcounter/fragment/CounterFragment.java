6130c3c7c8d6c198597e854a618b59a1273a6d7e
Aris <aristestpen03@gmail.com>
2020-12-30 09:09:52 +0700

    view model and live data

--- app/src/main/java/com/aries/courtcounter/fragment/CounterFragment.java
+++ app/src/main/java/com/aries/courtcounter/fragment/CounterFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
@@ -30,49 +31,13 @@ public class CounterFragment extends Fragment {
         // Inflate the layout for this fragment
         FragmentCounterBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_counter, container, false);
         viewModel = new ViewModelProvider(this).get(CounterViewModel.class);
+        binding.setLifecycleOwner(this);
         binding.setCounterViewModel(viewModel);
-
-        //Home
-
-        binding.btnThreePointHome.setOnClickListener(v -> {
-            viewModel.score.setHomeScore(viewModel.score.getHomeScore() + 3);
-            binding.txtHomeScore.setText(Integer.toString(viewModel.score.getHomeScore()));
-        });
-
-        binding.btnTwoPointHome.setOnClickListener(v -> {
-            viewModel.score.setHomeScore(viewModel.score.getHomeScore() + 2);
-            binding.txtHomeScore.setText(Integer.toString(viewModel.score.getHomeScore()));
-        });
-
-        binding.btnFreeThrowHome.setOnClickListener(v -> {
-            viewModel.score.setHomeScore(viewModel.score.getHomeScore() + 1);
-            binding.txtHomeScore.setText(Integer.toString(viewModel.score.getHomeScore()));
-        });
-
-
-        //Away
-        binding.btnThreePointAway.setOnClickListener(v -> {
-            viewModel.score.setAwayScore(viewModel.score.getAwayScore() + 3);
-            binding.txtScoreAway.setText(Integer.toString(viewModel.score.getAwayScore()));
-        });
-
-        binding.btnTwoPointAway.setOnClickListener(v -> {
-            viewModel.score.setAwayScore(viewModel.score.getAwayScore() + 2);
-            binding.txtScoreAway.setText(Integer.toString(viewModel.score.getAwayScore()));
-        });
-
-        binding.btnFreeThrowAway.setOnClickListener(v -> {
-            viewModel.score.setAwayScore(viewModel.score.getAwayScore() + 1);
-            binding.txtScoreAway.setText(Integer.toString(viewModel.score.getAwayScore()));
-        });
-
-        //Reset Score
-        binding.btnResetScore.setOnClickListener(v -> {
-            viewModel.score.setHomeScore(0);
-            viewModel.score.setAwayScore(0);
-            binding.txtHomeScore.setText(Integer.toString(viewModel.score.getHomeScore()));
-            binding.txtScoreAway.setText(Integer.toString(viewModel.score.getAwayScore()));
+        viewModel.scoreMutableLiveData.observe(getViewLifecycleOwner(), score -> {
+            binding.txtHomeScore.setText(Integer.toString(score.getHomeScore()));
+            binding.txtScoreAway.setText(Integer.toString(score.getAwayScore()));
         });
         return binding.getRoot();
+
     }
 }
--- app/src/main/java/com/aries/courtcounter/viewModel/CounterViewModel.java
+++ app/src/main/java/com/aries/courtcounter/viewModel/CounterViewModel.java
@@ -1,11 +1,28 @@
 package com.aries.courtcounter.viewModel;
 
+import android.util.Log;
+
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.aries.courtcounter.model.Score;
 
 public class CounterViewModel extends ViewModel {
 
+    public MutableLiveData<Score> scoreMutableLiveData = new MutableLiveData<>();
     public Score score = new Score(0, 0);
+
+    public void addPointHome(int point){
+        score.setHomeScore(score.getHomeScore()+point);
+        scoreMutableLiveData.setValue(score);
+        Log.d("Point Home",  String.valueOf(score.getHomeScore()));
+    }
+
+    public void addPointAway(int point){
+        score.setAwayScore(score.getAwayScore()+point);
+        scoreMutableLiveData.setValue(score);
+        Log.d("Point Away",  String.valueOf(score.getAwayScore()));
+
+    }
 }
 
